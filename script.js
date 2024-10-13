300e26e7ce45b83a7e2a8c18a756cad31d0e25eb
harshana47 <pabodhaharshana15.com>
2024-10-13 13:35:09 +0530

    done (almost) resizing 768px

--- script.js
+++ script.js
@@ -35,3 +35,32 @@ previousButton.onclick = function() {
         updateTransform();
     }
 };
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active')
+}
+
+const typed = new Typed('.multiple-text', {
+    strings: ['<i>Frontend Developer</i>.', '<i>Backend Developer</i>.', '<i>Web Developer</i>.'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay:1200,
+    loop:true,
+});
+
+const navLinks = document.querySelectorAll('.navbar a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
